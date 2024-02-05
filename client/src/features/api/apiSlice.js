fcb36d0bc93b5a17d7c0c1cf8042d1dc335f787d
Sarthak Sharma <sarthak2232@gmail.com>
2024-02-05 21:32:13 +0530

    back to first commit

--- client/src/features/api/apiSlice.js
+++ client/src/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://video-saver-server.vercel.app/db.json",
+    baseUrl: "http://localhost:5000",
   }),
   tagTypes: ["Videos", "Video", "RelatedVideos"],
   endpoints: (builder) => ({
