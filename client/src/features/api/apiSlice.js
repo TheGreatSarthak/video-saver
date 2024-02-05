6a9c15d9d539ccfe1711c6bed4990fa66bfddfc9
Sarthak Sharma <sarthak2232@gmail.com>
2024-02-05 16:35:38 +0530

    added json url

--- client/src/features/api/apiSlice.js
+++ client/src/features/api/apiSlice.js
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://video-saver-server.vercel.app/db.json",
+  }),
   tagTypes: ["Videos", "Video", "RelatedVideos"],
   endpoints: (builder) => ({
     getVideos: builder.query({
